fe1a98631de890f86ad18219107e6be651ca5734
agustinmend <jose.mendoza.u1@ucb.edu.bo>
2026-08-19 02:44:36 -0400

    modificacion de app.py con host

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # isw233-despliegue-mendoza
+## Uso de IA
+-investigar estructura de archivos para flask
+-elaboracion de css
+-correccion de etiquetas html
+## Levantamiento del servicor sin docker
+-En primera no corrio sospecho del debuger active que me imprime la consola
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route('/', methods=['GET'])
+def saludar():
+    return "Hello world!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
